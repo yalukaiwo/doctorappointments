fd3bdb0fe88687db05552037509a4d40b2712d0b
Luka Ponomarenko <Luka@Ponomarenkos-MacBook-Pro.local>
2021-06-14 12:55:19 +0300

    s

--- gulp-task/clean.js
+++
@@ -1,9 +0,0 @@
-const { src } = require("gulp")
-const clean = require("gulp-clean")
-
-const cl = () => {
-    return src('./dist/', {read: false})
-        .pipe(clean());
-}
-
-exports.clean = cl
