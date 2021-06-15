c203a7361a6fd61f68ac44a0a718ec0625ecd026
Даниил Щербаков <djdaniil46@gmail.com>
2021-06-15 16:13:39 +0300

    creation is ready for all classes

--- src/js/zApp.js
+++ src/js/zApp.js
@@ -1,2 +1,6 @@
-const visit = new Cardiologist(123, 123, 412, 124, 12, 142, 124, 124, 124, 24);
+const visit = new Cardiologist(123, "Cardio", 412, 124, 12, 142, 124, 124, 124, 24);
 visit.post();
+const dent = new Dentist("aa","Dentist","da","d","d","d","sad","s")
+dent.post();
+const den2t = new Therapist("aa", "a", "da", "d", "d", "d", "sad", "s");
+den2t.post();
