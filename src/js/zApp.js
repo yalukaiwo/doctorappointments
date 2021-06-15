df1f560fda4928555a49bd072399f121e9369111
Даниил Щербаков <djdaniil46@gmail.com>
2021-06-15 13:29:56 +0300

    something fixed & idk

--- src/js/zApp.js
+++ src/js/zApp.js
@@ -1,24 +1,2 @@
-<<<<<<< HEAD
-const form = document.querySelector(".modal__form");
-const submit = document.getElementById("modalFormSubmit");
-
-console.dir(form);
-// submit.addEventListener("click", (evt) => {
-//   evt.preventDefault();
-//   const visit = new Visit(
-//     form[0].value,
-//     form[1].value,
-//     form[2].value,
-//     form[3].value,
-//     form[4].value,
-//     form[5].value
-//   );
-//   visit.post();
-// });
 const visit = new Cardiologist(123, 123, 412, 124, 12, 142, 124, 124, 124, 24);
 visit.post();
-// visit.render();
-=======
-const visit = new Cardiologist(123, 123, 412, 124, 12, 142, 124, 124, 124, 24);
-visit.post();
->>>>>>> dev
