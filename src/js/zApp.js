a8147bc72166b588899714184e06ed0808e7a5b6
Даниил Щербаков <djdaniil46@gmail.com>
2021-06-15 04:22:16 +0300

    classes finished-creation working

--- src/js/zApp.js
+++ src/js/zApp.js
@@ -1,39 +1,16 @@
-// const { Visit } = require("./classes.js");
-// class Visit {
-//   constructor(id, name, doctor, importancy, goal, description, date) {
-//     this.name = name;
-//     this.doctor = doctor;
-//     this.importancy = importancy;
-//     this.goal = goal;
-//     this.description = description;
-//     this.date = date;
-//     this.id = id;
-//   }
-//   async post() {
-//     let response = await axios({
-//       method: "post",
-//       url: "https://ajax.test-danit.com/api/v2/cards",
-//       data: {
-//         title: "Визит к кардиологу",
-//         description: "Плановый визит",
-//         doctor: "Cardiologist",
-//         bp: "24",
-//         age: 23,
-//         weight: 70,
-//       },
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer 04a749a6-0cb8-43ca-9511-6bc6d5fa9396`,
-//       },
-//     });
-//     this.description = response.data.description;
-//     this.id = response.data.id;
-//     console.log(response);
-//     console.log(this.description);
-//   }
-//   render() {}
-//   edit() {}
-//   delete() {}
-// }
-const test = new Visit();
-test.post();
+const form = document.querySelector(".modal__form");
+const submit = form[12];
+
+console.dir(form);
+submit.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const visit = new Visit(
+    form[0].value,
+    form[1].value,
+    form[2].value,
+    form[3].value,
+    form[4].value,
+    form[5].value
+  );
+  visit.post();
+});
